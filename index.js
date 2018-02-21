e502af67fbf642d3f71c01e3b128b47f47feb086
Mohamed Ayman <M.Ayman2612@gmail.com>
2018-02-21 17:38:18 +0200

    ADD json format

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var express = require('express'),
+    json = require('express-json');
+ 
+var app = express()
+  .use(json())
+  .use(function (req, res) {
+    res.json({
+        message: 'Hello World!'
+    });
+  })
+  .listen(3000);
